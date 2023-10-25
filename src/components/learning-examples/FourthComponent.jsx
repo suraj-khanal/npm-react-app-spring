aa68e6737e3573e9729953f8d251c79b87ec979b
suraj-khanal <113327166+suraj-khanal@users.noreply.github.com>
2023-10-24 23:24:38 -0400

    First Commit

---
+++ src/components/learning-examples/FourthComponent.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export default class FourthComponent extends Component{
+    render(){
+      return (
+        <div className="Fourth Component">Fourth Component</div>
+      );
+    }
+}
